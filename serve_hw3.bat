4e9165ae80038ea54c0ac2d3c059ad9498a8d3c6
ht155 <tong_haoyang@outlook.com>
2022-02-22 20:03:09 -0500

    1

---
+++ serve_hw3.bat
@@ -0,0 +1,4 @@
+cd C:/Users/ht155/PycharmProjects/app
+git pull https://%TESTAPP_GIT_PAT%@github.com/wyatt-tong/homework_3-1.git
+venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe server.py
